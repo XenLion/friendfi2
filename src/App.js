5a5a4ebe58905c11eaee0ea660fae16bcec6680f
XenLion <ajene.hallbarrett@gmail.com>
2022-09-12 20:16:19 -0400

    installed and configured react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Home from "./Pages/Home/Home";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+          <Route path="/">
+              <Route index element={<Home/>}/>
+          </Route>
+      </Routes>
     </div>
   );
 }
